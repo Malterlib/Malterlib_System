93707b418055d6c6d8694e791f01485880bb1e25
Erik Olofsson <erik.olofsson@favro.com>
2018-12-08 19:05:39 +0100

    [Malterlib/System] Refactor namespaces

--- Source/Malterlib_System_CPUUsage.h
+++ Source/Malterlib_System_CPUUsage.h
@@ -1,50 +1,46 @@
-﻿// Copyright © 2015 Hansoft AB 
+// Copyright © 2015 Hansoft AB 
 // Distributed under the MIT license, see license text in LICENSE.Malterlib
 
 #pragma once
 
 #include <Mib/Core/Core>
 
-namespace NMib
+namespace NMib::NSystem
 {
-	namespace NSystem
+	class CCPUUsage
 	{
-		class CCPUUsage
-		{
 
-			void *m_pHandle;
-		public:
-			CCPUUsage();
-			~CCPUUsage();
+		void *m_pHandle;
+	public:
+		CCPUUsage();
+		~CCPUUsage();
 
-			CSystemCPUUsage f_GetUsage(bool &_bChanged);
-			CSystemCPUUsage f_GetUsage();
-		};		
+		CSystemCPUUsage f_GetUsage(bool &_bChanged);
+		CSystemCPUUsage f_GetUsage();
+	};
 
-		class CCPUUsageMonitor : protected NThread::CThread
-		{
-			CCPUUsage m_CPUUsage;
-			fp32 m_SampleInterval;
-			zmint m_CurrentHistory;
-			zmint m_nHistory;
-			NContainer::TCVector<CSystemCPUUsage> m_History;
-			NThread::CMutual m_Lock;
-			CSystemCPUUsage m_Last;
-			CSystemCPUUsage m_Average;
-			CSystemCPUUsage m_Min;
-			CSystemCPUUsage m_Max;
-			aint f_Main();
-			NStr::CStr f_GetThreadName();
-		public:
-			CCPUUsageMonitor(fp32 _SampleInterval, fp32 _AverageInterval);
-			~CCPUUsageMonitor();
+	class CCPUUsageMonitor : protected NThread::CThread
+	{
+		CCPUUsage m_CPUUsage;
+		fp32 m_SampleInterval;
+		zmint m_CurrentHistory;
+		zmint m_nHistory;
+		NContainer::TCVector<CSystemCPUUsage> m_History;
+		NThread::CMutual m_Lock;
+		CSystemCPUUsage m_Last;
+		CSystemCPUUsage m_Average;
+		CSystemCPUUsage m_Min;
+		CSystemCPUUsage m_Max;
+		aint f_Main();
+		NStr::CStr f_GetThreadName();
+	public:
+		CCPUUsageMonitor(fp32 _SampleInterval, fp32 _AverageInterval);
+		~CCPUUsageMonitor();
 
-			CSystemCPUUsage f_GetLast();
+		CSystemCPUUsage f_GetLast();
 
-			CSystemCPUUsage f_GetAverage();
-			CSystemCPUUsage f_GetMin();
-			CSystemCPUUsage f_GetMax();
-		};
-	}
+		CSystemCPUUsage f_GetAverage();
+		CSystemCPUUsage f_GetMin();
+		CSystemCPUUsage f_GetMax();
+	};
 }
-
